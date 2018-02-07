bc523517357c17c9575af5531e3bac81db4fb919
Alex Tomasello <alex.tomasello@gmail.com>
2018-02-07 12:36:28 +0100

    Update ntp.js

--- ntp.js
+++ ntp.js
@@ -5,7 +5,7 @@ var Sender = new ZabbixSender({host: '127.0.0.1'});
 
 ntpClient.getNetworkTime(process.argv[2], 123, function(err, date) {
         if(err) {
-            Sender.addItem(process.argv[3], 'ntp.value', "error"]);
+            Sender.addItem(process.argv[3], 'ntp.value', "error");
         } else {
 		    Sender.addItem(process.argv[3], 'ntp.value', date);
         }
