274d57b7e489a7158058ed674fbc0d396b153ddb
Alex Tomasello <alex.tomasello@axians.it>
2018-02-07 12:22:08 +0100

    Changed to zabbix trapper

--- ntp.js
+++ ntp.js
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
 var ntpClient = require('ntp-client');
+var ZabbixSender = require('node-zabbix-sender');
+var Sender = new ZabbixSender({host: '127.0.0.1'});
 
 ntpClient.getNetworkTime(process.argv[2], 123, function(err, date) {
         if(err) {
-                console.log("error");
-                return;
+            Sender.addItem(process.argv[3], 'ntp.value', "error"]);
+        } else {
+		    Sender.addItem(process.argv[3], 'ntp.value', date);
         }
-        console.log(date); // Mon Jul 08 2013 21:31:31 GMT+0200 (Paris, Madrid (heure dâÃ©tÃ©))
+		Sender.send();
 });
 
